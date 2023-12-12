e84ce8eaba0565c55c8ea16c496febc1a676c3c3
Esra Şen <sen.esra@yahoo.com>
2023-12-12 14:59:49 +0300

    Sayilari kücükten büyüğe siralama

---
+++ SayiSiralama/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //değişkenleri tanımladık
+        int a, b, c;
+        Scanner input = new Scanner(System.in);
+
+        //kullanıcıdan değerleri aldık
+        System.out.print(" 1. sayıyı giriniz: ");
+        a= input.nextInt();
+        System.out.print(" 2. sayıyı giriniz: ");
+        b= input.nextInt();
+        System.out.print(" 3. sayıyı giriniz: ");
+        c= input.nextInt();
+
+        //sayıları karşılaştırıp sıraladık
+        if((a<b) && (a<c)){
+            if(b<c){
+                System.out.println("a<b<c");
+            }
+            else{
+                System.out.println("a<c<b");
+            }
+        }
+        else if((b<a) && (b<c)){
+            if(a<c){
+                System.out.println("b<a<c");
+            }
+            else{
+                System.out.println("b<c<a");
+            }
+        }
+        else if((c<a) && (c<b)){
+            if(a<b){
+                System.out.println("c<a<b");
+            }
+            else{
+                System.out.println("c<b<a");
+            }
+        }
+        else{
+            System.out.println("a=b=c");
+        }
+    }
+}
